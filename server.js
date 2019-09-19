fdcc870c7fd43a28d81ae8e26159a964cf1cbfe1
nmom0001 <54693420+nmom0001@users.noreply.github.com>
2019-09-19 16:16:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const url = "mongodb://localhost:27017/";
 let db;
 
 //Connect to mongoDB server
-MongoClient.connect(url, { useNewUrlParser: true },
+MongoClient.connect(url, { useNewUrlParser: true ,unifiedTopology: true},
     function (err, client) {
         if (err) {
             console.log("Err  ", err);
